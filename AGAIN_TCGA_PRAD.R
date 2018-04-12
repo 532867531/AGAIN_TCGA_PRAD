7ea4fba709da0533746d7694e078e3efbc2b2f6b
532867531 <532867531@qq.com>
2018-04-12 15:30:49 +0800

    连续性组合前的上传

--- AGAIN_TCGA_PRAD.R
+++ AGAIN_TCGA_PRAD.R
@@ -16,7 +16,7 @@ ori_low=67;ori_middle=260;ori_high=171##CXCL17
 ori_low=118;ori_middle=206;ori_high=174##cxcl15
 ori_low=37;ori_middle=46;ori_high=34##cxcl15
 getGene0="CXCL17"
-getGene0="CXCL5"
+#getGene0="CXCL5"
 #getGene0="PML"
 ref_Gene="ZBTB7A"
 ref_Gene1="PTEN"
@@ -74,9 +74,9 @@ shell.exec(paste(out_put_dir,sep=""))
 
 ###开始进行聚类
 fix(rna_seq_data)
-geneSignatures=c("ANPEP","CD38","CSF3","CXCR2","CD80","IDO","SLEB2","CD274","PTPRC","TLR3","CEACAM8","STAT1","CXCR4","LOC116196","CSF1R","ITGAM","ITGAX","CD14","TGFB1","CXCL12","LOC199828","ENTPD1","FUT4","STAT3","IL4R","STAT5A","TLR4","CSF2","CXCL8","S100A8","S100A9","TNF")
-# geneSignatures0=c("ARG1","CCL2","CD40","IL4R","FCGR2A","CD68","CD33","CSF1R","ITGAM","HLA-DRB1","FOXP3","PDCDILG2","CD163","LY75","PTPRC","CCR2","CD200R1","IL10","FCER2")
-# geneSignatures=c("ARG1","CCL2","CD40","IL4R","FCGR2A","CD68","LOC116196","CSF1R","ITGAM","HLA-DRB1","JM2","PDCD1LG2","CD163","LY75","PTPRC","LOC90262","CD200R1","IL10","FCER2")
+#geneSignatures=c("ANPEP","CD38","CSF3","CXCR2","CD80","IDO","SLEB2","CD274","PTPRC","TLR3","CEACAM8","STAT1","CXCR4","LOC116196","CSF1R","ITGAM","ITGAX","CD14","TGFB1","CXCL12","LOC199828","ENTPD1","FUT4","STAT3","IL4R","STAT5A","TLR4","CSF2","CXCL8","S100A8","S100A9","TNF")
+geneSignatures0=c("ARG1","CCL2","CD40","IL4R","FCGR2A","CD68","CD33","CSF1R","ITGAM","HLA-DRB1","FOXP3","PDCDILG2","CD163","LY75","PTPRC","CCR2","CD200R1","IL10","FCER2")
+geneSignatures=c(getGene0,"ARG1","CCL2","CD40","IL4R","FCGR2A","CD68","LOC116196","CSF1R","ITGAM","HLA-DRB1","JM2","PDCD1LG2","CD163","LY75","PTPRC","LOC90262","CD200R1","IL10","FCER2")
 data2use=rna_seq_data[geneSignatures,];
 rownames(data2use)=geneSignatures;fix(data2use)
 ###完成了数据的提取，下面进行数据的预处理，分为两部分，复制出一部分进行data_plot
@@ -93,9 +93,9 @@ library(lattice)
 #win.graph();densityplot(unlist(data_plot));Sys.sleep(5);dev.off()
 {"进行log2处理！"}
 data_plot=log2(data_plot+1)
-win.graph();densityplot(unlist(data_plot));Sys.sleep(5);dev.off()
+#win.graph();densityplot(unlist(data_plot));Sys.sleep(5);dev.off()
 {"进行scale处理"}
-data_plot=data.frame(t(scale(t(data_plot)+1)))
+#data_plot=data.frame(t(scale(t(data_plot),center =TRUE,scale = TRUE)))
 #win.graph();densityplot(unlist(data_plot));Sys.sleep(5);dev.off()
 
 ####开始进行热图的绘制
@@ -119,7 +119,7 @@ goHeatmap=function(d1=data_plot,distmethod="euclidean",clustmethod="average"){
                scale="none",
                trace="none",
                dendrogram = "column",
-               Rowv = FALSE,
+               Rowv = TRUE,
                Colv = TRUE,
                sepcolor = "white",
                symkey = TRUE,
@@ -127,7 +127,7 @@ goHeatmap=function(d1=data_plot,distmethod="euclidean",clustmethod="average"){
                # breaks = c(min(d1)-0.5,seq(-3,3,0.005),max(d1)+0.5)
                breaks = BREAKS,
                main = paste("distmethod",distmethod,"clustermethod",clustmethod,sep="_")
-               #,lmat=rbind(c(0,3,0),c(0,1,2),c(0,4,0)) ,lhei=c(3,10,3),lwid=c(1,9,1)
+               #,lmat=rbind(c(0,3,0),c(0,1,2),c(0,4,0)) ,lhei=c(3,10,3),lwid=c(0,9,1)
   )
   return(hm)
 }
@@ -140,22 +140,23 @@ Cluster_Method<-c(
   "ward.D2",
   #"single",
   "complete",
-  "average"# ,
+  "average" ,
   #"mcquitty",
   #"median",
-  #"centroid"
+  "centroid"
 )
 Dist_Methods<-  c("euclidean"
-                  #, "maximum", "manhattan", 
-                  #"canberra", 
+                  #, "maximum"
+                  , "manhattan" 
+                  #,"canberra", 
                   #"binary", 
                   #"minkowski",
                   #"pearson", "spearman","kendall"
 )
 
-for(a in dev.list()){
-  dev.off()
-}
+# for(a in dev.list()){
+#   dev.off()
+# }
 
 if(FALSE){
 for(onedistmethod in Dist_Methods){
@@ -167,12 +168,13 @@ for(onedistmethod in Dist_Methods){
 
 
 
+
 goboxplot=function(d2=data_plot,getGene0_=getGene0,one_dist_method,one_clust_method){
   ###??????????????????
   library(factoextra)
     dist=get_dist(x=t(d2),method = one_dist_method)
     hc=hclust(d=dist,method = one_clust_method)
-    hcc=data.frame(cutree(tree = hc,k=7));colnames(hcc)="treeIndex"
+    hcc=data.frame(cutree(tree = hc,k=3));colnames(hcc)="treeIndex"
     hcc[,"Sample"]=rownames(hcc)
     data_getGene0=data.frame(t(rna_seq_data[getGene0_,])[c(-1,-2),]);colnames(data_getGene0)="Value";data_getGene0[,"Sample"]=rownames(data_getGene0)
     hcc=merge(hcc,data_getGene0,by.x = "Sample",by.y ="Sample",all = TRUE )
@@ -197,7 +199,7 @@ goboxplot=function(d2=data_plot,getGene0_=getGene0,one_dist_method,one_clust_met
     hcc2$rank_group_index=paste("rank_group_index",hcc2$rank_group_index,sep = "_")
      ##合并重新分组后的
     hcc=merge(hcc,hcc2,by.x ="groupMean",by.y="groupMean")
-    hcc$Value=as.numeric(hcc$Value)
+    hcc$Value=log2(as.numeric(hcc$Value))
     for(one in unique(hcc$rank_group_index)){
       hcc[which(hcc$rank_group_index==one),"group_count"]=length(which(hcc$rank_group_index==one))
     }
@@ -224,10 +226,9 @@ goboxplot=function(d2=data_plot,getGene0_=getGene0,one_dist_method,one_clust_met
   library(ggpubr);
   ##十分之一percent，用于绘图
     particle=max(diff(hcc$Value))/10
-    #win.graph();
-    fix(hcc)
+        fix(hcc)
     q=qplot(data = hcc,x=rank_group_index,y=hcc$Value,geom = "boxplot",outlier.colour = "black",outlier.colour="black",
-                        ylab = getGene0_,main = paste("dist_method",one_dist_method,"hclust_method",one_clust_method,sep = "_"))
+                        ylab = paste("Log2(",getGene0_,")",sep = ""),main = paste("dist_method",one_dist_method,"hclust_method",one_clust_method,sep = "_"))
     q=q+ geom_jitter(aes(colour = rank_low2high))
     q=q+geom_text(data = hcc,aes(label=paste("n=",group_count,sep=""),y=min(hcc$Value)-particle))
     q=q+stat_compare_means(aes(label=paste0(..method..,"\n", "p=",..p.format..)),
@@ -276,80 +277,3 @@ plot_together=function(){
 
 
 
-
-
-
-if(FALSE){
-
-#提取zbtb7a
-data_refGene=rna_seq_data[ref_Gene,]
-if("Hugo_Symbol" %in% colnames(rna_seq_data)){
-  data_refGene=data_refGene[,-which(colnames(data_refGene)=="Hugo_Symbol")] 
-}
-fix(data_refGene)
-##进行参考基因的分组
-criteria_refGene.high=as.numeric(quantile(data_refGene,1-0.205))
-criteria_refGene.low=as.numeric(quantile(data_refGene,1-0.795))
-
-Data_refGene.high=data_refGene[,which(data_refGene>=criteria_refGene.high)]
-Data_refGene.low=data_refGene[,which(data_refGene<=criteria_refGene.low)]
-
-Sample_refGene.high=colnames(Data_refGene.high)
-Sample_refGene.high=Sample_refGene.high[which(Sample_refGene.high %in% colnames(rna_seq_data))]
-Sample_refGene.low=colnames(Data_refGene.low)
-Sample_refGene.low=Sample_refGene.low[which(Sample_refGene.low %in% colnames(rna_seq_data))]
-
-
-##开始获取signature基因
-data_signatire=rna_seq_data[getGene0,]
-
-tryCatch({
-  if("Entrez_Gene_Id" %in% colnames(rna_seq_data)){
-    data_signatire=data_signatire[,-which(colnames(data_signatire)=="Entrez_Gene_Id")]  
-  }
-  if("Hugo_Symbol" %in% colnames(rna_seq_data)){
-    data_signatire=data_signatire[,-which(colnames(data_signatire)=="Hugo_Symbol")]
-  }
-},finally = {
-  "全部去除完毕!"
-})
-
-##查看未找到的名称
-rownames(data_signatire)=getGene0
-fix(data_signatire)
-print(summary(is.na(rownames(data_signatire))))
-##寻找criteria基因的样品进行分组,绘图时加标签进行分组
-lable_Func_preProcess=function(){print("对signature基因进行分组处理！")}
-Sample_Alt_Normal=data_CNA[which(data_CNA$Hugo_Symbol==ref_Gene1),]
-rownames(Sample_Alt_Normal)=Sample_Alt_Normal$Hugo_Symbol
-if( "Hugo_Symbol"%in%colnames(Sample_Alt_Normal)){
-  Sample_Alt_Normal=Sample_Alt_Normal[,-which(colnames(Sample_Alt_Normal)=="Hugo_Symbol")]
-}
-if("Entrez_Gene_Id"%in%colnames(Sample_Alt_Normal)){
-  Sample_Alt_Normal=Sample_Alt_Normal[,-which(colnames(Sample_Alt_Normal)=="Entrez_Gene_Id")]
-}
-
-Sample_Alt=rownames(as.data.frame(t(Sample_Alt_Normal)[which(Sample_Alt_Normal==-2),]))
-Sample_Alt=Sample_Alt[which(Sample_Alt %in% colnames(rna_seq_data))]
-Sample_Normal=rownames(as.data.frame(t(Sample_Alt_Normal)[which(abs(Sample_Alt_Normal)<2),]))
-Sample_Normal=Sample_Normal[which(Sample_Normal %in% colnames(rna_seq_data))]
-##画图
-data_plot=as.data.frame(t(data_signatire))
-data_plot[Sample_Alt,"ALT"]=paste(ref_Gene1,"ALT",sep = "_")
-data_plot[Sample_Normal,"ALT"]=paste(ref_Gene1,"Normal",sep = "_")
-data_plot[Sample_refGene.low,paste(ref_Gene,"Level",sep="_")]=paste(ref_Gene,"Low",sep = "_")
-data_plot[Sample_refGene.high,paste(ref_Gene,"Level",sep="_")]=paste(ref_Gene,"High",sep = "_")
-library(ggpubr);library(ggrepel)
-secondMin=min(data_plot[,getGene0][which(data_plot[,getGene0]!=0)])
-data_plot[,getGene0]=log2(data_plot[,getGene0]+secondMin)
-write.csv(x=data_plot,file = paste(out_put_dir,"tcga_signature.csv",sep=""))
-
-
-#可视化DEPDC1基因表达谱
-ggboxplot(data=data_plot,x="ALT",y="CXCL5", 
-          color = "ALT",add = "jitter", legend="none")+ 
-  
-  geom_hline(yintercept = mean(data_plot[,getGene0]), linetype=2)+# Add horizontal line at base mean 
-  stat_compare_means(method = "t.test") # Add global annova p-value 
-win.graph();plot(q)
-}
