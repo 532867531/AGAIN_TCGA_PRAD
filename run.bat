218a17370c39a6007100a6733528a2c13129866e
532867531 <532867531@qq.com>
2018-04-25 00:42:08 +0800

    runbat

---
+++ run.bat
@@ -0,0 +1,17 @@
+::Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PMN	SLC30A1
+REM ::Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	Mo	SLC30A1
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	T	SLC30A1
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PI3K_AKT_mTOR	SLC30A1
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PMN	SLC30A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	Mo	SLC30A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	T	SLC30A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PI3K_AKT_mTOR	SLC30A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PMN	SLC39A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	Mo	SLC39A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	T	SLC39A10
+REM Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PI3K_AKT_mTOR	SLC39A10
+Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PMN	SLC39A14	 
+Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	Mo	SLC39A14	
+Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	T	SLC39A14	 
+Rscript.exe --vanilla --verbose 	换做TCGA_LIHC的数据.R 	PI3K_AKT_mTOR	SLC39A14
+shutdown -s -t 60
